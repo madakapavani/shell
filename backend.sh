3fd46a64406be33df35b4d40493e6587b6e41ad7
pavani <pvr9602@gmail.com>
2025-01-09 19:44:03 +0530

    project

--- backend.sh
+++ backend.sh
@@ -73,7 +73,7 @@ cp /home/ec2-user/shell/backend.service /etc/systemd/system/backend.service
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h mysql.172.31.22.161 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+mysql -h mysql.172.31.87.225 -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Setting up the transactions schema and tables"
 
 systemctl daemon-reload &>>$LOG_FILE_NAME
